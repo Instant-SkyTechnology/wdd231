702dcd648ae28b0bd48b70a91fb4defc961a07d8
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-11-04 15:35:39 -0500

    Fixed some bugs

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -5,6 +5,7 @@ async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
     console.table(data.prophets); // temporary testing of data response
+    displayProphets(data.prophets);
 }
 
 
