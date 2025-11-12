bba3e11cf171a6f0d6a7a85f8835c0af291c371f
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-11-12 16:21:04 -0500

    Fixed some bugs

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -23,57 +23,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const humidity = document.querySelector('#humidity');
     const sunriseTime = document.querySelector('#sunrise');
     const sunsetTime = document.querySelector('#sunset');
-    const captionDesc = document.querySelector('figcaption');
+    // const captionDesc = document.querySelector('figcaption');
     const forecastContainer = document.querySelector('#forecast-container');
     // CREATE REQUIRED VARIABLES FOR THE URL
     const myKey = "56edcb845dab93e7e886ee7d7b38aee5";
     const myLat = "43.76315701698374";
     const myLon = "-79.4242287044823";
 
-    // CONSTRUCT A FULL PATH TEMPLATE LITERAL
-    // const myURL = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=metric`;
-    // const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=metric`;
-    // THE CURRENT WEATHER DATA
-    // async function apiFetch() {
-    //     try {
-    //         const response = await fetch(myURL);
-    //         if (response.ok) {
-    //             const data = await response.json();
-    //             console.log(data); // testing only
-    //             displayResults(data);
-    //         } else {
-    //             throw Error(await response.text());
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-
-    // // DISPLAY JSON DATA ONTO THE WEB PAGE
-    // function displayResults(data) {
-    //     myTown.innerHTML = data.name;
-    //     currentTemp.innerHTML = `<strong>${data.main.temp}&deg;</strong> C`;
-    //     // myDescription.innerHTML = data.weather[0].description;
-
-    //     const desc = data.weather[0].description;
-    //     captionDesc.textContent = desc;
-
-    //     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    //     weatherIcon.setAttribute('src', iconsrc);
-    //     weatherIcon.setAttribute('alt', data.weather[0].description);
-
-    //     highTemp.innerHTML = `High: ${data.main.temp_max.toFixed(1)}&deg;C`;
-    //     lowTemp.innerHTML = `Low: ${data.main.temp_min.toFixed(1)}&deg;C`;
-    //     humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-
-    //     // Convert sunrise and sunset (UNIX timestamps → local time)
-    //     const sunriseDate = new Date(data.sys.sunrise * 1000);
-    //     const sunsetDate = new Date(data.sys.sunset * 1000);
-    //     const options = { hour: '2-digit', minute: '2-digit' };
-
-    //     sunriseTime.innerHTML = `Sunrise: ${sunriseDate.toLocaleTimeString([], options)}`;
-    //     sunsetTime.innerHTML = `Sunset: ${sunsetDate.toLocaleTimeString([], options)}`;
-    // }
 
     // WEATHER + FORECAST URLs
     const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=metric`;
@@ -112,11 +68,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // DISPLAY CURRENT WEATHER
     function displayResults(data) {
         myTown.innerHTML = data.name;
-        // myDescription.innerHTML = data.weather[0].description;
+        myDescription.innerHTML = data.weather[0].description;
         currentTemp.innerHTML = `<strong>${data.main.temp}&deg;</strong> C`;
 
-        const desc = data.weather[0].description;
-        captionDesc.textContent = desc;
+        // const desc = data.weather[0].description;
+        // captionDesc.textContent = desc;
 
         const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
         weatherIcon.setAttribute('src', iconsrc);
