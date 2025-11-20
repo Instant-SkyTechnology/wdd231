1225f09c4817708578b9bfc3397741628340ed09
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-11-19 23:17:57 -0500

    Fixed some bugs

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -21,8 +21,69 @@ document.getElementById("timestamp").textContent = myInfo.get("timestamp")
     ? new Date(myInfo.get("timestamp")).toLocaleString()
     : "N/A";
 
+// const npModal = document.querySelector("#npModal");
+// const bronzeModal = document.querySelector("#bronzeModal");
+// const silverModal = document.querySelector("#silverModal");
+// const goldModal = document.querySelector("#goldModal");
 
+// const dialogBox = document.querySelector("#dialogBox");
+// const closeButton = document.querySelector("#closeButton");
+// const dialogBoxText = document.querySelector("#dialogContent");
 
+// npModal.addEventListener("click", () => {
+//     dialogBox.showModal();
+//     dialogBoxText.innerHTML = `Special events and networking.`
+// });
 
+// bronzeModal.addEventListener("click", () => {
+//     dialogBox.showModal();
+//     dialogBoxText.innerHTML = `Includes advertising & event discounts.`
+// });
 
+// silverModal.addEventListener("click", () => {
+//     dialogBox.showModal();
+//     dialogBoxText.innerHTML = `Enhanced advertising, training, and event access.`
+// });
+
+// goldModal.addEventListener("click", () => {
+//     dialogBox.showModal();
+//     dialogBoxText.innerHTML = `All perks, spotlight features, and premium events.`
+// });
+
+// closeButton.addEventListener("click", () => {
+//     dialogBox.close();
+// });
+
+const npMember = document.querySelector("#npMember");
+const bronzeMember = document.querySelector("#bronzeMember");
+const silverMember = document.querySelector("#silverMember");
+const goldMember = document.querySelector("#goldMember");
+
+const dialogBox = document.querySelector("#dialogBox");
+const closeButton = document.querySelector("#closeButton");
+const dialogContent = document.querySelector("#dialogContent");
+
+npMember.addEventListener("click", () => {
+    dialogContent.textContent = "Special events and networking.";
+    dialogBox.showModal();
+});
+
+bronzeMember.addEventListener("click", () => {
+    dialogContent.textContent = "Includes advertising & event discounts.";
+    dialogBox.showModal();
+});
+
+silverMember.addEventListener("click", () => {
+    dialogContent.textContent = "Enhanced advertising, training, and event access.";
+    dialogBox.showModal();
+});
+
+goldMember.addEventListener("click", () => {
+    dialogContent.textContent = "All perks, spotlight features, and premium events.";
+    dialogBox.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    dialogBox.close();
+});
 
